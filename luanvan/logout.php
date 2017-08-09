82ebbedae9deb7fe8e11154b52c57b0650be58ce
trangtanphuoc <30283697+trangtanphuoc@users.noreply.github.com>
2017-08-09 14:32:09 -0700

    fwffz

--- luanvan/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-	session_start();
-	//unset($_SESSION['username']);
-	session_destroy();
-	echo $_SESSION['username'];
-	$_SESSION['username']='';
-	header("Location: index.php");
-	exit();
-?>
